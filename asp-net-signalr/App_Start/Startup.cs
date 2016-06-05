5147ad2f4d13c5d0410699f9b039f2564fcdec4b
chicobentojr <francisco.bento.jr@hotmail.com>
2016-06-05 16:59:03 -0300

    Finish the simple tutorial
    Fix the Roslyn problem with AppHarbor

---
+++ asp-net-signalr/App_Start/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(asp_net_signalr.Startup))]
+
+namespace asp_net_signalr
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ asp-net-signalr/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_net_signalr.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
